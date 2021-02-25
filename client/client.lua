0b393c2b82c17695e9df028709408f9fc1d020a9
Diackk <79240389+Diackk@users.noreply.github.com>
2021-02-25 19:33:25 +0000

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -259,7 +259,7 @@ function IsPlayerNearCoords(x, y, z)
     local playerx, playery, playerz = table.unpack(GetEntityCoords(GetPlayerPed(), 0))
     local distance = GetDistanceBetweenCoords(playerx, playery, playerz, x, y, z, true)
 
-    if distance < 1 then
+    if distance < 5 then
         return true
     end
 end
@@ -273,6 +273,8 @@ local currentZone = nil
 local blips = {
     --Blackwater
     { name = 'Barber', sprite = -2090472724, x = -812.67, y = -1366.81, z = 43.75 },
+    { name = 'Barber', sprite = -2090472724, x = -307.45, y = 812.17, z = 118.98 },
+    { name = 'Barber', sprite = -2090472724, x = 2655.69, y = -1179.83, z = 53.28 },
 
 }
 
@@ -365,7 +367,16 @@ Citizen.CreateThread(function()
                 elseif Config.menuChoice == false then
                     barberMenu()
                 end
-                TaskStartScenarioAtPosition(GetPlayerPed(), GetHashKey("PROP_PLAYER_BARBER_SEAT"), -815.3, -1367.018, 43.50, 90.60, 0, 0, 1)
+
+
+                if IsPlayerNearCoords(-814.263, -1364.779, 43.750) then
+                    TaskStartScenarioAtPosition(GetPlayerPed(), GetHashKey("PROP_PLAYER_BARBER_SEAT"), -815.3, -1367.018, 43.50, 90.60, 0, 0, 1)
+                elseif IsPlayerNearCoords(-307.45, 812.17, 118.98) then
+                    TaskStartScenarioAtPosition(GetPlayerPed(), GetHashKey("PROP_PLAYER_BARBER_SEAT"), -306.62, 813.56, 118.75, 90.60, 0, 0, 1)
+                elseif IsPlayerNearCoords(2655.05, -1179.92, 53.28) then
+                    TaskStartScenarioAtPosition(GetPlayerPed(), GetHashKey("PROP_PLAYER_BARBER_SEAT"), 2655.38, -1180.92, 53.00, 182.8, 0, 0, 1)
+                end
+
                          
                 PromptSetEnabled(ShopPrompt, false)
                 PromptSetVisible(ShopPrompt, false)
